d4c96755cdccf79ca36422bd2766f32730619e2b
anshul singh bisht <anshul.bisht06@gmail.com>
2018-03-24 20:23:09 +0530

    root files path updated for different envs

--- apis_django/apis/apis/envs.py
+++ apis_django/apis/apis/envs.py
@@ -1,10 +1,15 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 config = {
 	'development': {
 		'DEBUG': True,
-		'ALLOWED_HOSTS': ['localhost']
+		'ALLOWED_HOSTS': ['localhost'],
+		'STATIC_ROOT': os.path.join(BASE_DIR, 'staticfiles')
 	},
 	'production': {
 		'DEBUG': False,
-		'ALLOWED_HOSTS': ['18.188.98.228']
+		'ALLOWED_HOSTS': ['18.188.98.228'],
+		'STATIC_ROOT': '/var/www/ayushi/static/'
 	}
 }
